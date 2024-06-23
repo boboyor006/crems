b3a31b017b47ef4633cb20e07b67ce1148de11aa
temur006 <162977738+boboyor006@users.noreply.github.com>
2024-06-23 11:36:56 +0500

    davom etrimaiza
    pauza

---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <>
+    <Nav>
+    <Button type='button ' class='btn btn-outline-primary' >Primary</Button>
+    </Nav>
+    </>
+  )
+}
+
+export default Navbar; 
