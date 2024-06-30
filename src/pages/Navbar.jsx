7eff84c7b379c34cfb4e68021ce403dc86f803db
temur006 <162977738+boboyor006@users.noreply.github.com>
2024-06-30 10:10:39 +0500

    davmo bor
    kutub xonani navbar qismini qilishdi

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,23 +2,19 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
+import  "../css/navbar.css" 
+import Button from '@mui/material/Button';
 
 
 const Navbar = () => {
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" >
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
+      <Box   sx={{ flexGrow: 1 }}>
+      <AppBar position="static"  >
+        <Toolbar variant="dense" className='Box'>
+        
+          <Button className='Iconbutton' variant="contained">login </Button>
         </Toolbar>
       </AppBar>
     </Box>
