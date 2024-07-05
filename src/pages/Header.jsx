edf1e8976adef649c30b4dab0dc789f5899a38dd
temur006 <162977738+boboyor006@users.noreply.github.com>
2024-07-05 15:17:30 +0500

    resposnive
    endi respisove kere

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,21 +3,46 @@ import React from 'react'
 import "../css/header.css"
 // import Toolbar from '@mui/material'
 // import Box from '@mui/material'
-import "../data/img.js"
-
+import heders from "../img/headerImg.svg"
 const Header = () => {
+
+
+  const h3 = {
+     width: "400px",
+     fontfamil: "san-serif",
+     marginTop : "100px",
+     marginLeft: "80px",
+  } 
+
+
+  const p = {
+    width: "350px", 
+    color: "rgb(110, 106, 106)",
+    marginLeft: "80px",
+  }
+
+
   return (
     <>
         <header className='AppBar'>
 
 
           <div className="header_text">
-
+            <h1 style={h3}>
+            orzu jamoalari
+            shu yerda yaratilgan
+            </h1>
+            <p style={p}>
+            ta'lim sohasida ish va xodimlarni topish bo'yicha yordamchi
+            </p>
+            <button className='heder_btn'>
+              Batafsil 
+            </button>
           </div>
 
-          
-              <div className="header_img">
 
+              <div className="header_img">
+                <img src={heders} alt="" />
               </div>
         </header>
     </>
